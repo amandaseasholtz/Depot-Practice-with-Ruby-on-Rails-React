621775dd9624887623e924b13c3da657f25d2c14
amandaseasholtz <alseasholtz@valdosta.edu>
2019-02-03 16:47:24 -0500

    Cleaned up code from last commit - cart link in left menu bar goes to YOUR cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,17 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
   before_action :set_cart
  
   def index
     @products = Product.order(:title)
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-		def set_cart
-			@cart = Cart.find(session[:cart_id])
-		rescue ActiveRecord::RecordNotFound
-			@cart = Cart.create
-			session[:cart_id] = @cart.id
-		end
-  end
-
+end
