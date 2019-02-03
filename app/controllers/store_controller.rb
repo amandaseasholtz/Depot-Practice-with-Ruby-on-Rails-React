2731f4cd74740fcf9e49c300f14c9d6571d18ac9
amandaseasholtz <alseasholtz@valdosta.edu>
2019-02-03 15:58:37 -0500

    Fixed so that cart link in left menu bar goes to YOUR cart and not the generic cart page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  before_action :set_cart
+ 
   def index
     @products = Product.order(:title)
   end
-end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+		def set_cart
+			@cart = Cart.find(session[:cart_id])
+		rescue ActiveRecord::RecordNotFound
+			@cart = Cart.create
+			session[:cart_id] = @cart.id
+		end
+  end
+
