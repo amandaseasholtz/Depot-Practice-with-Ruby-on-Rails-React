5289041a24f5f46371f4f503d34ba6a3c29fca2d
amandaseasholtz <alseasholtz@valdosta.edu>
2019-02-04 20:45:05 -0500

    Added count and a pop up prompting you to buy if you visit the page many times

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,5 +4,19 @@ class StoreController < ApplicationController
  
   def index
     @products = Product.order(:title)
-  end
-end
+    @counter = session_counter
+  end 
+  def session_counter  
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter]+=1
+    end 
+
+    if session[:counter] >= 5
+        flash.notice = "You have been here #{session[:counter]} 
+          times. Please go ahead and buy something "
+      render:index
+      end
+    end
+  end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  get 'carts/:id', to: 'carts#show', as: 'currentCart'
   resources :products
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
