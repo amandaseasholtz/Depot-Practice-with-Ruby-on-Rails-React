2c1e742828eb3150892b3f832b3c4dbda80a4627
amandaseasholtz <alseasholtz@valdosta.edu>
2019-02-21 02:46:10 -0500

    Prepared for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     end
    end
 
+   mount ActionCable.server => '/cable'
+   
   resources :products
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
