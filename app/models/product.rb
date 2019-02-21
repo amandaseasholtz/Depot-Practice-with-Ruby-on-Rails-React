83fd259be972c0cde8bb73124ecd3d7007a092a6
amandaseasholtz <alseasholtz@valdosta.edu>
2019-02-21 08:17:54 -0500

    Commented out image validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
     mount_uploader :image_url, ImageUploader
     validates :title, :description, :image_url, :popularity, presence: true
     validates :title, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-        with:
-        %r{\.(gif|jpg|png)\Z}i,
-        message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+    #validates :image_url, allow_blank: true, format: {
+     #   with:
+     #   %r{\.(gif|jpg|png)\Z}i,
+     #   message: 'must be a URL for GIF, JPG or PNG image.'
+    #}
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
     private
